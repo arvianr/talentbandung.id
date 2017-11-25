b62b52e05a80e1674ecf620766992f5a684586ad
arvianrachmadi <arvianrachmadi@gmail.com>
2017-11-25 13:12:43 +0700

    apa ya

--- artmosthere.com/js/main.js
+++ artmosthere.com/js/main.js
@@ -95,7 +95,7 @@ $(document).ready(function(){
 		// slidesToShow: 1;
 		responsive: true,
 		appendArrows:$('.arrow'),
-		nextArrow: '<button type="button" class="slick-next"><i class="ion-chevron-right"></button>',
-		prevArrow: '<button type="button" class="slick-prev"><i class="ion-chevron-left"></button>'
+		nextArrow: '<button type="button" class="slick-next"><i class="fa fa-chevron-right" aria-hidden="true"></i></button>',
+		prevArrow: '<button type="button" class="slick-prev"><i class="fa fa-chevron-left" aria-hidden="true"></i></button>'
 	});
 });
