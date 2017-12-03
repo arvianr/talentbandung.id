40e10c763cfd4f72b05011743aaaeca2119f0f6c
arvianrachmadi <arvianrachmadi@gmail.com>
2017-12-03 16:48:35 +0700

    revisi

--- artmosthere.com/js/slide.js
+++ artmosthere.com/js/slide.js
@@ -41,51 +41,15 @@ jQuery(document).ready(function ($) {
 
 jQuery(document).ready(function ($) {
 
-	var slideCount = $('#slider2 ul li').length;
-	var slideWidth = $('#slider2 ul li').width();
-	var slideHeight = $('#slider2 ul li').height();
-	var sliderUlWidth = slideCount * slideWidth;
-
-	$('#slider2').css({ width: slideWidth, height: slideHeight });
-
-	$('#slider2 ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
-
-    $('#slider2 ul li:last-child').prependTo('#slider2 ul');
-
-    function moveLeft() {
-        $('#slider2 ul').animate({
-            left: + slideWidth
-        }, 500, function () {
-            $('#slider2 ul li:last-child').prependTo('#slider2 ul');
-            $('#slider2 ul').css('left', '');
-        });
-    };
-
-    function moveRight() {
-        $('#slider2 ul').animate({
-            left: - slideWidth
-        }, 500, function () {
-            $('#slider2 ul li:first-child').appendTo('#slider2 ul');
-            $('#slider2 ul').css('left', '');
-        });
-    };
-
-    $('a.control_prev_2').click(function () {
-        moveLeft();
-    });
-
-    $('a.control_next_2').click(function () {
-        moveRight();
-    });
-
 		$('.hov').mouseover(function(){
 			$('.icon-about figure img').css("opacity", "0");
-			$('.detail-about h3').css("color", "white");
+			$('.detail-about h3').css("opacity", "0");
 		});
 
 		$('div.hov').mouseout(function(){
 			$('.icon-about figure img').css("opacity", "1");
-			$('.detail-about h3').css("color", "black");
+			$('.detail-about h3').css("opacity", "1");
 		});
+
 
 });
